f882056ccfb2a020d30457b06fa820d318d93ccf
Константин Ямшанов <yamshanov@dev.vtb.ru>
2023-08-06 10:27:02 +0400

    simple gateway

---
+++ gateway/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.1.3-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.2"
+	kotlin("jvm") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22"
+}
+
+group = "ru.kyamshanov.mission"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+extra["springCloudVersion"] = "2022.0.4"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ gateway/src/main/kotlin/ru/kyamshanov/mission/gateway/SecurityConfig.kt
@@ -0,0 +1,22 @@
+package ru.kyamshanov.mission.gateway
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig {
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http
+            .authorizeExchange { it.anyExchange().authenticated() }
+            .oauth2ResourceServer { r -> r.jwt(Customizer.withDefaults()) }
+            .csrf(Customizer.withDefaults())
+            .build()
+    }
+}
