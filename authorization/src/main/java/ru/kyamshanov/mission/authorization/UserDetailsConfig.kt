ed559c84aecceac906c6df28a94cf095fc06e87d
Константин Ямшанов <yamshanov@dev.vtb.ru>
2023-08-06 21:40:05 +0400

    support refresh token

--- authorization/src/main/java/ru/kyamshanov/mission/authorization/UserDetailsConfig.kt
+++ authorization/src/main/java/ru/kyamshanov/mission/authorization/UserDetailsConfig.kt
@@ -3,17 +3,8 @@ package ru.kyamshanov.mission.authorization
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.oauth2.core.AuthorizationGrantType
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod
-import org.springframework.security.oauth2.core.oidc.OidcScopes
-import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
-import org.springframework.security.oauth2.server.authorization.settings.ClientSettings
 import org.springframework.security.provisioning.JdbcUserDetailsManager
-import ru.kyamshanov.mission.authorization.repositories.AuthorizationConsentRepository
 import ru.kyamshanov.mission.authorization.repositories.ClientRepository
-import java.util.*
 import javax.sql.DataSource
 
 
@@ -24,10 +15,5 @@ class UserDetailsConfig {
     fun userDetailsService(
         dataSource: DataSource,
         clientRepository: ClientRepository
-    ): UserDetailsService {
-
-        println("TEEST: ${clientRepository.findByClientId("desktop-client")}")
-
-        return JdbcUserDetailsManager(dataSource)
-    }
+    ): UserDetailsService = JdbcUserDetailsManager(dataSource)
 }
--- database/assets/init_db/mission-authorization-db-creating-script.sql
+++ database/assets/init_db/mission-authorization-db-creating-script.sql
@@ -99,9 +99,9 @@ VALUES ('user', 'ROLE_DEFAULT');
 INSERT INTO client (id, clientId, clientIdIssuedAt, clientSecret, clientSecretExpiresAt, clientName,
                     clientAuthenticationMethods, authorizationGrantTypes, redirectUris, postLogoutRedirectUris, scopes,
                     clientSettings, tokenSettings)
-VALUES (0, 'desktop-client', '2023-08-01 00:00:00.000000', '{noop}secret', null, 'desktop-client',
-        'client_secret_basic', 'authorization_code, refresh_token',
+VALUES ('oidc-client', 'desktop-client', '2023-08-01 00:00:00.000000', '{noop}secret', null, 'oidc-client',
+        'client_secret_basic', 'refresh_token,authorization_code',
         'http://127.0.0.1:8080/desktop/authorized',
-        'http://127.0.0.1:8080/desktop/logged-out','openid',
+        'http://127.0.0.1:8080/desktop/logged-out', 'openid',
         '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":true,"settings.client.require-authorization-consent":true}',
         '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":true,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",300.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000],"settings.token.device-code-time-to-live":["java.time.Duration",300.000000000]}');
