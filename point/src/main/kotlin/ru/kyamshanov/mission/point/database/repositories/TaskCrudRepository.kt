84402079fc47ffd4c162d9f086438f251663fa38
Константин Ямшанов <yamshanov@dev.vtb.ru>
2023-10-15 19:19:16 +0400

    added the ability to update the task status

--- point/src/main/kotlin/ru/kyamshanov/mission/point/database/repositories/TaskCrudRepository.kt
+++ point/src/main/kotlin/ru/kyamshanov/mission/point/database/repositories/TaskCrudRepository.kt
@@ -5,6 +5,7 @@ import org.springframework.data.r2dbc.repository.Modifying
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import ru.kyamshanov.mission.point.domain.models.TaskEntity
+import ru.kyamshanov.mission.point.domain.models.TaskStatus
 import ru.kyamshanov.mission.point.domain.models.TaskType
 
 
@@ -17,4 +18,8 @@ interface TaskCrudRepository : CoroutineCrudRepository<TaskEntity, String> {
     @Modifying
     @Query("update tasks set type = :type where id = :taskId and owner = :ownerId RETURNING *;")
     fun updateTaskType(taskId: String, ownerId: String, type: TaskType?): Flow<TaskEntity>
+
+    @Modifying
+    @Query("update tasks set status = :status where id = :taskId and owner = :ownerId RETURNING *;")
+    fun updateTaskStatus(taskId: String, ownerId: String, status: TaskStatus): Flow<TaskEntity>
 }
