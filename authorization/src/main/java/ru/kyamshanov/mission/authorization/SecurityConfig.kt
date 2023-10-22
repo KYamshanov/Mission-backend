9f91f7f24da2bb3785d077f9afd9c4a5b6b5a384
Константин Ямшанов <yamshanov@dev.vtb.ru>
2023-10-22 11:36:24 +0400

    security remove JSESSION

--- authorization/src/main/java/ru/kyamshanov/mission/authorization/SecurityConfig.kt
+++ authorization/src/main/java/ru/kyamshanov/mission/authorization/SecurityConfig.kt
@@ -5,9 +5,24 @@ import com.nimbusds.jose.jwk.RSAKey
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet
 import com.nimbusds.jose.jwk.source.JWKSource
 import com.nimbusds.jose.proc.SecurityContext
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.crypto.codec.Hex
+import org.springframework.core.annotation.Order
+import org.springframework.http.MediaType
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.Authentication
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
+import org.springframework.security.web.savedrequest.NullRequestCache
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher
 import java.security.KeyPair
 import java.security.KeyPairGenerator
 import java.security.interfaces.RSAPrivateKey
@@ -30,6 +45,38 @@ class SecurityConfig {
             .build()
         val jwkSet = JWKSet(rsaKey)
         return ImmutableJWKSet<SecurityContext>(jwkSet)
+    }
+
+    @Bean
+    @Order(1)
+    @Throws(java.lang.Exception::class)
+    fun authorizationServerSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer::class.java)
+            .oidc(Customizer.withDefaults()) // Enable OpenID Connect 1.0
+        http // Redirect to the login page when not authenticated from the
+            // authorization endpoint
+            .exceptionHandling { exceptions: ExceptionHandlingConfigurer<HttpSecurity?> ->
+                exceptions
+                    .defaultAuthenticationEntryPointFor(
+                        LoginUrlAuthenticationEntryPoint("/login"),
+                        MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                    )
+            }
+        return http.build()
+    }
+
+    @Bean
+    @Order(2)
+    @Throws(java.lang.Exception::class)
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { authorize ->
+                authorize.anyRequest().authenticated()
+            } // Form login handles the redirect to the login page from the
+            // authorization server filter chain
+            .formLogin(Customizer.withDefaults())
+        return http.build()
     }
 
     private fun generateRsaKey(): KeyPair {
