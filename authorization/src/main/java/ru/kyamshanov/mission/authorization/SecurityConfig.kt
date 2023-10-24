722ba00340688897b3c1f50f8c586970766c5285
Константин Ямшанов <yamshanov@dev.vtb.ru>
2023-10-24 09:11:59 +0400

    f

--- authorization/src/main/java/ru/kyamshanov/mission/authorization/SecurityConfig.kt
+++ authorization/src/main/java/ru/kyamshanov/mission/authorization/SecurityConfig.kt
@@ -10,18 +10,25 @@ import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
+import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
-import org.springframework.security.web.savedrequest.NullRequestCache
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
+import org.springframework.security.web.context.SecurityContextRepository
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher
 import java.security.KeyPair
 import java.security.KeyPairGenerator
@@ -32,6 +39,93 @@ import java.util.*
 
 @Configuration
 class SecurityConfig {
+
+    @Bean
+    fun customAuthenticationSuccessHandler() = object :
+        SavedRequestAwareAuthenticationSuccessHandler() {
+
+        override fun onAuthenticationSuccess(
+            request: HttpServletRequest,
+            response: HttpServletResponse,
+            authentication: Authentication
+        ) {
+            super.onAuthenticationSuccess(request, response, authentication)
+            println("Success login fdfdfsdf")
+            SecurityContextHolder.clearContext()
+        }
+
+
+    }
+
+    @Bean
+    @Order(1)
+    @Throws(java.lang.Exception::class)
+    fun authorizationServerSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer::class.java)
+            .oidc(Customizer.withDefaults()) // Enable OpenID Connect 1.0
+        http
+            // Redirect to the login page when not authenticated from the
+            // authorization endpoint
+            .exceptionHandling { exceptions: ExceptionHandlingConfigurer<HttpSecurity?> ->
+                exceptions
+                    .defaultAuthenticationEntryPointFor(
+                        object : AuthenticationEntryPoint {
+                            override fun commence(
+                                request: HttpServletRequest,
+                                response: HttpServletResponse,
+                                authException: AuthenticationException?
+                            ) {
+                                response.status = HttpStatus.OK.value()
+                                response.writer.println("dsdsad")
+                            }
+
+                        },
+                        /*
+                                                LoginUrlAuthenticationEntryPoint("/login").apply { setUseForward(true) },
+                        */
+                        MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                    )
+            }
+            // Accept access tokens for User Info and/or Client Registration
+            .oauth2ResourceServer { resourceServer: OAuth2ResourceServerConfigurer<HttpSecurity?> ->
+                resourceServer.jwt(Customizer.withDefaults())
+            }
+        return http.build()
+    }
+
+    @Bean
+    @Order(2)
+    @Throws(java.lang.Exception::class)
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { authorize ->
+                authorize.requestMatchers("/oauth2/authorize").authenticated()
+                    .anyRequest().denyAll()
+            }
+            // Form login handles the redirect to the login page from the
+            // authorization server filter chain
+            .formLogin {
+                /*  it
+                      .successHandler(
+                          object :
+                              SavedRequestAwareAuthenticationSuccessHandler() {
+
+                              override fun onAuthenticationSuccess(
+                                  request: HttpServletRequest,
+                                  response: HttpServletResponse,
+                                  authentication: Authentication
+                              ) {
+                                  super.onAuthenticationSuccess(request, response, authentication)
+                                  println("Success login fdfdfsdf")
+                                  request.getSession(false).invalidate()
+                              }
+                          }
+                      )*/
+            }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+        return http.build()
+    }
 
 
     @Bean
@@ -45,38 +139,6 @@ class SecurityConfig {
             .build()
         val jwkSet = JWKSet(rsaKey)
         return ImmutableJWKSet<SecurityContext>(jwkSet)
-    }
-
-    @Bean
-    @Order(1)
-    @Throws(java.lang.Exception::class)
-    fun authorizationServerSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
-        http.getConfigurer(OAuth2AuthorizationServerConfigurer::class.java)
-            .oidc(Customizer.withDefaults()) // Enable OpenID Connect 1.0
-        http // Redirect to the login page when not authenticated from the
-            // authorization endpoint
-            .exceptionHandling { exceptions: ExceptionHandlingConfigurer<HttpSecurity?> ->
-                exceptions
-                    .defaultAuthenticationEntryPointFor(
-                        LoginUrlAuthenticationEntryPoint("/login"),
-                        MediaTypeRequestMatcher(MediaType.TEXT_HTML)
-                    )
-            }
-        return http.build()
-    }
-
-    @Bean
-    @Order(2)
-    @Throws(java.lang.Exception::class)
-    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .authorizeHttpRequests { authorize ->
-                authorize.anyRequest().authenticated()
-            } // Form login handles the redirect to the login page from the
-            // authorization server filter chain
-            .formLogin(Customizer.withDefaults())
-        return http.build()
     }
 
     private fun generateRsaKey(): KeyPair {
