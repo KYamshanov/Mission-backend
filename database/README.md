a0b9d6b64695cafed00eb6115d23ad501622d550
KYamshanov <kyamshanov@vk.com>
2024-01-19 14:13:10 +0400

    automation of database deployment

--- database/README.md
+++ database/README.md
@@ -1,3 +1,5 @@
 Docker-compose для запуска центарлизованной БД
 
 Для подключения к БД : psql -U postgres -h localhost postgres
+
+Run deploy_script.sh at windows available by using Git Bash
---
+++ database/deploy_script.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ ! -d ./assets ]; then
+    echo "directory ./assets not found!"
+    exit
+fi
+
+if [ ! -f postgres_db.txt ]; then
+    echo "File postgres_db.txt not found!"
+    exit
+fi
+
+if [ ! -f postgres_user.txt ]; then
+    echo "File postgres_user.txt not found!"
+    exit
+fi
+
+if [ ! -f postgres_password.txt ]; then
+    echo "File postgres_password.txt not found!"
+    exit
+fi
+
+assets="./assets"
+mkdir ./deploy
+curl https://raw.githubusercontent.com/KYamshanov/Mission-backend/develop/database/docker-compose.yml --output ./deploy/docker-compose.yml
+docker compose up --abort-on-container-exit
+rm -r ./deploy
