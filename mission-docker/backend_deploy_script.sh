26683c083a963357b0ca323d4cf02db042d18a9d
KYamshanov <kyamshanov@vk.com>
2024-01-20 17:30:25 +0400

    backend deploy script

---
+++ mission-docker/backend_deploy_script.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ ! -d ./secrets ]; then
+    echo "directory ./secrets not found!"
+    exit
+fi
+
+mkdir ./backend
+curl https://raw.githubusercontent.com/KYamshanov/Mission-backend/develop/mission-id/docker-stack.yml --output ./backend/docker-stack.yml
+mkdir ./backend/secrets
+cp -r ./secrets ./backend/
+cd ./backend
+docker stack deploy -c ./docker-stack.yml backend
+cd ..
+rm -r ./backend
